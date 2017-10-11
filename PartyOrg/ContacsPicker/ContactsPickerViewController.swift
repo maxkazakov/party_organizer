3e14da929a1ba93647e73584a76bd92aa5b94c93
Max Kazakov <kazakov.maixm.a@gmail.com>
2017-10-11 15:36:02 +0500

    avatar icon

--- PartyOrg/ContacsPicker/ContactsPickerViewController.swift
+++ PartyOrg/ContacsPicker/ContactsPickerViewController.swift
@@ -16,31 +16,44 @@ class ContactsPickerViewController: UITableViewController {
     private var granted = false
     private var contacts: [MemberViewData] = []
     
+    func cancel() {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    func done() {
+        self.dismiss(animated: true, completion: nil)
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(AccessDeniedCell.self, forCellReuseIdentifier: AccessDeniedCell.identifier)
-        
+        navigationItem.leftBarButtonItem =  UIBarButtonItem(image: #imageLiteral(resourceName: "cancel_bar"), style: .plain, target: self, action: #selector(cancel))
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(done))
         title = "SELECT_MEMBERS.CONTACTS".tr()
 
         let store = CNContactStore()
+        
         store.requestAccess(for: .contacts) { isGranted, error in
             self.granted = isGranted
-
+            
             if isGranted {
                 self.fetchContacts { contacts in
                     for contact in contacts {
-                        var contactItem = MemberViewData(name: CNContactFormatter.string(from: contact, style: .fullName)!, phone: contact.phoneNumbers.first?.value.stringValue ?? "")
-                        contactItem.avatar = contact.thumbnailImageData.flatMap (UIImage.init)                        
+                        var contactItem = MemberViewData(name: CNContactFormatter.string(from: contact, style: .fullName) ?? "", phone: contact.phoneNumbers.first?.value.stringValue ?? "")
+                        contactItem.avatar = contact.thumbnailImageData.flatMap (UIImage.init) ?? self.getDummyImage(by: contactItem.name)
                         self.contacts.append(contactItem)
                     }
+                    
+                    self.contacts.sort(by: {a, b in b.name > a.name })
                     self.tableView.reloadData()
                 }
             }
         }
     }
 
+    
+    
     
     
     private func fetchContacts(block: @escaping ([CNContact]) -> Void) {
@@ -82,7 +95,7 @@ class ContactsPickerViewController: UITableViewController {
         if granted {
             let cell = tableView.dequeueReusableCell(withIdentifier: ContactViewCell.identifier) as! ContactViewCell
             let contact = contacts[indexPath.row]
-            cell.setup(name: contact.name, phone: contact.phone, avatar: nil)
+            cell.setup(name: contact.name, phone: contact.phone, avatar: contact.avatar)
             return cell
         }
         
@@ -92,6 +105,39 @@ class ContactsPickerViewController: UITableViewController {
     
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return granted ? 70 : 150
+        return granted ? 76 : 150
     }
+    
+    
+    
+    private func getDummyImage(by name: String) -> UIImage {
+        let color = getColor()
+        let size = CGSize(width: 60, height: 60)
+        let rect = CGRect(origin: CGPoint(x: 0, y: 0), size: size)
+        UIGraphicsBeginImageContextWithOptions(size, true, UIScreen.main.scale)
+        let context = UIGraphicsGetCurrentContext()!        
+        context.setFillColor(color)
+        context.fill(rect)
+        let char = name.first ?? "U"
+        
+        let style = NSMutableParagraphStyle()
+        style.alignment = .center
+        let charStr = String(repeating: char, count: 1) as NSString
+        let textAttributes = [NSFontAttributeName: UIFont.boldSystemFont(ofSize: 24), NSForegroundColorAttributeName: UIColor.white, NSParagraphStyleAttributeName : style]
+        
+        let stringSize = charStr.size(attributes: textAttributes)
+        let stringPoint = CGPoint(x: (size.width - stringSize.width) / 2, y: (size.height - stringSize.height) / 2)
+        charStr.draw(at: stringPoint, withAttributes: textAttributes)
+        
+        let cgImage = context.makeImage()!
+        UIGraphicsEndImageContext()
+        return UIImage(cgImage: cgImage)
+    }
+    
+    private func getColor() -> CGColor {
+        let colors = Colors.contactColors
+        let index = Int(arc4random()) % colors.count
+        return colors[index].cgColor
+    }
+    
 }
